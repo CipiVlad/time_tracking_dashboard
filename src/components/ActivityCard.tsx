60c0054851245dac70aa59aa96560b8d3e4c3a6a
CipiVlad <supercoder.cipi@gmail.com>
2024-04-19 10:52:34 +0200

    mobile styling matched, heading to final steps

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -15,24 +15,25 @@ const ActivityCard = (props: Data & OptionTimeFrameProps) => {
                 <p className='activity'>{props.title}</p>
                 <div className='three_dots'>...</div>
             </div>
-            <h2 className="activity_time">
-                {
-                    props.optionTimeFrame === 'Daily'
-                        ? timeframes.daily.current
-                        : props.optionTimeFrame === 'Weekly'
-                            ? timeframes.weekly.current
-                            : timeframes.monthly.current
-                }hrs
-
-            </h2>
-            <p className="activity_previous">Last Week -
-                {
-                    props.optionTimeFrame === 'Daily'
-                        ? timeframes.daily.previous
-                        : props.optionTimeFrame === 'Weekly'
-                            ? timeframes.weekly.previous
-                            : timeframes.monthly.previous
-                }hrs</p>
+            <div className="activity_time_flex_container">
+                <h2 className="activity_time">
+                    {
+                        props.optionTimeFrame === 'Daily'
+                            ? timeframes.daily.current
+                            : props.optionTimeFrame === 'Weekly'
+                                ? timeframes.weekly.current
+                                : timeframes.monthly.current
+                    }hrs
+                </h2>
+                <p className="activity_previous">Last Week -
+                    {
+                        props.optionTimeFrame === 'Daily'
+                            ? timeframes.daily.previous
+                            : props.optionTimeFrame === 'Weekly'
+                                ? timeframes.weekly.previous
+                                : timeframes.monthly.previous
+                    }hrs</p>
+            </div>
         </div>
     )
 }
